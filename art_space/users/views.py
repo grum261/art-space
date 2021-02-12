52b049ac9cbcf9cd7933e5555ad91d19d93119a6
grum261 <grum231@yahoo.com>
2021-02-12 14:58:20 +0500

    Update serializers, views and urls

--- art_space/users/views.py
+++ art_space/users/views.py
@@ -8,30 +8,39 @@ from .models import Profile
 
 
 class RegisterUserView(generics.CreateAPIView):
+    '''Страница регистрации нового пользователя: /api/users/registration/'''
     permission_classes = [AllowAny, ]
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
 
 
 class ChangeUserPasswordView(generics.UpdateAPIView):
+    '''
+    Страница изменения пароля пользователя: /api/users/{user_id}/change_password/
+
+    Создать и добавить права на изменения только самим собой
+    '''
     permission_classes = [IsAuthenticated, ]
     queryset = User.objects.all()
     serializer_class = ChangeUserPasswordSerializer
 
 
 class ListProfileView(generics.ListAPIView):
+    '''Страница просмотра пользовательских профилей: /api/users/profiles/'''
     permission_classes = [AllowAny, ]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class CreateProfileView(generics.CreateAPIView):
+    '''Страница создания профиля зарегистрированного пользователя: /api/users/profiles/new/'''
     permission_classes = [IsAuthenticated, ]
     queryset = Profile.objects.all()
-    serializer = ProfileSerializer
+    serializer_class = ProfileSerializer
 
 
 class RetrieveUpdateDestroyProfileView(generics.RetrieveUpdateDestroyAPIView):
+    '''Страница изменения/удаления профиля пользователя: /api/users/profiles/{profile_id}/'''
     permission_classes = [IsAuthenticated, ]
     queryset = Profile.objects.all()
-    serializer = ProfileSerializer
+    serializer_class = ProfileSerializer
