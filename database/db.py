21d1c66e779eb921ed1a00785529d6819cc4e864
grum261 <38054569+grum261@users.noreply.github.com>
2021-02-18 17:28:59 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,4 +1,5 @@
 import databases
+import os
 
 from datetime import datetime
 from sqlalchemy import MetaData, Boolean, Column, Integer, String, DateTime, Text, ForeignKey, Table
@@ -6,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from sqlalchemy import create_engine
 
 
-DATABASE_URL = 'postgresql://grum231:jeJiqNxu6nqC@localhost:5432/art_space'
+DATABASE_URL = os.getenv('db_url')
 
 database = databases.Database(DATABASE_URL)
 
